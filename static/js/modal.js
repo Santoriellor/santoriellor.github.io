a5eddbdaa2f916a98dd30afe1730de09e76a80d2
Santoriellor <154046978+Santoriellor@users.noreply.github.com>
2024-05-23 10:32:05 +0200

    Update modal.js

--- static/js/modal.js
+++ static/js/modal.js
@@ -15,6 +15,13 @@ $(document).ready(function() {
           'This website is meant as a tool for workskop to digitalize input from work report in order to ease the process of editing invoice and following stocks.',
         link: 'http://santoriellor.pythonanywhere.com/'
       },
+      comparator: {
+        title: 'Price comparator',
+        tag: 'Price comparator using Python',
+        detail:
+          'This website is scraping data from different online shop and presenting them for the user to compare prices.',
+        link: 'http://195.179.228.168/products'
+      },
     };
   
     $('#gallery .button').on('click', function() {
